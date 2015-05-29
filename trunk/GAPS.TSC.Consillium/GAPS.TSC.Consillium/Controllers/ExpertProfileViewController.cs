3f0afaab478f5515cb50ee52e6ff60fb6bf5c1e9
AnktiaGupta18 <angupta@greenapplesolutions.com>
2015-05-29 18:50:59 +0530

    Views Modification

---
+++ trunk/GAPS.TSC.Consillium/GAPS.TSC.Consillium/Controllers/ExpertProfileViewController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GAPS.TSC.Consillium.Controllers
+{
+    public class ExpertProfileViewController : Controller
+    {
+        //
+        // GET: /ExpertProfileView/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
