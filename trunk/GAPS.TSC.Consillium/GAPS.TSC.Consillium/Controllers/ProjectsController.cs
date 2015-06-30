e719b3e2ede34de9c442c3662b8ce1a12e7a00b8
AnktiaGupta18 <angupta@greenapplesolutions.com>
2015-06-30 12:48:56 +0530

    Screens

---
+++ trunk/GAPS.TSC.Consillium/GAPS.TSC.Consillium/Controllers/ProjectsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GAPS.TSC.Consillium.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        //
+        // GET: /Projects/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ExpertRequisition()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+        public ActionResult Payments()
+        {
+            return View();
+        }
+	}
+}
