c9e7a4eff6f1522342216f5ebeeb0250012aae66
AnktiaGupta18 <angupta@greenapplesolutions.com>
2015-06-30 12:51:04 +0530

    Controllers

---
+++ trunk/GAPS.TSC.Consillium/GAPS.TSC.Consillium/Controllers/EmployeesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GAPS.TSC.Consillium.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        //
+        // GET: /Employees/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddNewLead()
+        {
+            return View();
+        }
+        public ActionResult LeadsDashboard()
+        {
+            return View();
+        }
+        public ActionResult ExpertsDasboard()
+        {
+
+            return View();
+        }
+        public ActionResult Profile()
+        {
+
+            return View();
+        }
+        public ActionResult AddPls()
+        {
+
+            return View();
+        }
+        public ActionResult AddMembers()
+        {
+            return View();
+        }
+	}
+}
---
+++ trunk/GAPS.TSC.Consillium/GAPS.TSC.Consillium/Controllers/RequestsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GAPS.TSC.Consillium.Controllers
+{
+    public class RequestsController : Controller
+    {
+        //
+        // GET: /Requests/
+        public ActionResult RequestExpertEn()
+        {
+            return View();
+        }
+
+        public ActionResult RequestExpertManual()
+        {
+            return View();
+        }
+	}
+}
