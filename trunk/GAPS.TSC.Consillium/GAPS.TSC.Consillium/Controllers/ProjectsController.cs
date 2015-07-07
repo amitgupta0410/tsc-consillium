a1ac75535e0b92f76c8dc85537d00de56788a72b
kirti-vashishtha <kirti.vashishtha02@gmail.com>
2015-07-07 14:50:49 +0530

    Expert dashboard

--- trunk/GAPS.TSC.Consillium/GAPS.TSC.Consillium/Controllers/ProjectsController.cs
+++ trunk/GAPS.TSC.Consillium/GAPS.TSC.Consillium/Controllers/ProjectsController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GAPS.TSC.CONS.Domain;
 using GAPS.TSC.CONS.Services;
+using GAPS.TSC.CONS.Util;
+using GAPS.TSC.Consillium.Models;
 
 namespace GAPS.TSC.Consillium.Controllers
 {
@@ -11,16 +14,39 @@ namespace GAPS.TSC.Consillium.Controllers
     {
         //
         // GET: /Projects/
-        public ProjectsController(IAttachmentService attachmentService) : base(attachmentService) {}
+       // public ProjectsController(IAttachmentService attachmentService) : base(attachmentService) {}
+
+
+          private readonly IUserService _userService;
+          private readonly IClientService _clientService;
+
+          public ProjectsController(IUserService userService,IClientService clientService, IAttachmentService attachmentService )
+              : base(attachmentService)
+          {
+            
+            _userService = userService;
+            _clientService = clientService;
+          }
 
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult ExpertRequisition()
+        public ActionResult ExpertRequisition(ExpertRequisitionModel model)
         {
-            return View();
+            IEnumerable<SpecialProjectLeadMap> projectLead = new List<SpecialProjectLeadMap>();
+            projectLead = _userService.GetAllProjectLeads();
+           // model.ProjectLead = projectLead.ToDictionary(x)
+            model.StatusOptions = EnumHelper.GetEnumLabels(typeof(RequestStatus));
+            model.AssignedList = _userService.GetAllTeamMembers().ToDictionary(x => x.Id, x => x.Name);
+            
+            model.ClientList = _clientService.GetAllClients().ToDictionary(x => x.Id, x => x.Name);
+                   
+
+
+
+            return View(model);
         }
 
         public ActionResult Projects()
