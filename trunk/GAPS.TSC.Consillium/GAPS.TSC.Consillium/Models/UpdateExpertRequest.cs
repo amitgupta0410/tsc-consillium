3de1d7bf6c19d81b47614d6770d78ae072b855c8
AnktiaGupta18 <angupta@greenapplesolutions.com>
2015-07-14 13:46:00 +0530

    Update_RequestManual_request

--- trunk/GAPS.TSC.Consillium/GAPS.TSC.Consillium/Models/UpdateExpertRequest.cs
+++ trunk/GAPS.TSC.Consillium/GAPS.TSC.Consillium/Models/UpdateExpertRequest.cs
@@ -27,26 +27,28 @@ namespace GAPS.TSC.Consillium.Models
         public Dictionary<int, string> ProjectList { get; set; }
 
         public Dictionary<int, string> Clients { get; set; }
-       [Required(ErrorMessage = "Please select a Client")]
-        public int ClientId { get; set; }
+      
+        public int? ClientId { get; set; }
         public Dictionary<int, string> Units { get; set; }
-        public int UnitId { get; set; }
-        [Required(ErrorMessage = "Please select a Project")]
-        public int ProjectId { get; set; }
-      [Required(ErrorMessage = "Please select a ProjectLead")]
+        public int? UnitId { get; set; }
+      
+        public int? ProjectId { get; set; }
+      
         public int? ProjectLeadId { get; set; }
         public string ProjectLeadName { get; set; }
-        [Required(ErrorMessage = "Please enter BD Lead ")]
+       
         public int? BdLeadId { get; set; }
+        [Required(ErrorMessage = "Please enter BDLead name.")]
         public string BdLeadName { get; set; }
         public string Description { get; set; }
         public Dictionary<int, string> Industry { get; set; }
+        [Required(ErrorMessage = "Please Select Industry Field.")]
         public int IndustryId { get; set; }
         public Dictionary<int, string> Geography { get; set; }
-       [Required(ErrorMessage = "Please Select Geography Field.")]
+        [Required(ErrorMessage = "Please Select Geography Field.")]
         public int GeographicId { get; set; }
         public Dictionary<int, string> Currency { get; set; }
-       [Required(ErrorMessage = "Please Select Currency Field.")]
+        [Required(ErrorMessage = "Please Select Currency Field.")]
         public int BudgetCurrencyId { get; set; }
         public string Comments { get; set; }
         [Required(ErrorMessage = "Please Select Amount Field.")]
@@ -59,11 +61,7 @@ namespace GAPS.TSC.Consillium.Models
         [AssertThat("(CostSharingTypeValue == CostSharingType.TSC && ClientShare == 0 && TscShare== 100) || (CostSharingTypeValue == CostSharingType.Client && ClientShare == 100 && TscShare == 0) || (CostSharingTypeValue == CostSharingType.Both && ClientShare+TscShare== 100) || (CostSharingTypeValue == CostSharingType.ManDayBilling && ClientShare == 0 && TscShare == 0)", ErrorMessage = "Please select a valid share")]
 
         public decimal ClientShare { get; set; }
-
-       // [Required(ErrorMessage = "Please upload a file.")]
         public HttpPostedFileBase ScopingDocumentFile { get; set; }
-
-       // [Required(ErrorMessage = "Please upload a file.")]
         public HttpPostedFileBase ApprovalDocumentFile { get; set; }
         public int ScopingDocumentId { get; set; }
         public int ApprovalDocumentId { get; set; }
